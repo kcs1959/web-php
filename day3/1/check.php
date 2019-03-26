1faef39590a0a47c77fbf0dadc94d21dfcecb40c
ogiwara <yushiogiwara@gmail.com>
2019-03-26 21:27:07 +0900

    add day3

---
+++ day3/1/check.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
+<body>
+
+<?php
+$nickname =  htmlspecialchars($_POST["nickname"]);
+$email =  htmlspecialchars($_POST["email"]);
+$goiken =  htmlspecialchars($_POST["goiken"]);
+
+// string & int
+
+if($nickname == ''){
+  print "ニックネームが入力されてません！";
+}else{
+  print "ようこそ";
+  print $nickname;
+  print "様";
+  print "<br />";
+}
+
+if($email == ""){
+  print "メールアドレスが入力されていません";
+}else{
+  print "メールアドレス";
+  print $email;
+  print "<br />";
+}
+
+if($goiken == ""){
+  print "ご意見が入力されていません";
+}else{
+  print "ご意見";
+  print $goiken;
+  print "<br />";
+}
+
+if($nickname == "" || $email == "" || $goiken == ""){
+  print "<form>";
+  print "<input type='button' onclick='history.back()'  value='戻る' />";
+  print "</form>";
+
+}else{
+
+  print "<form method='post' action='thanks.php'>";
+
+  print '<input name="nickname" type="hidden" value="'. $nickname .'">';
+  print '<input name="email" type="hidden" value="'. $email .'">';
+  print '<input name="goiken" type="hidden" value="'. $goiken .'">';
+
+  print "<input type='button' onclick='history.back()'  value='戻る' />";
+  print "<input type='submit' value='OK' />";
+  print "</form>";
+}
+
+?>
+
+</body>
+</html>
---
+++ day3/1/thanks.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
+<body>
+
+<?php
+$dsn = "mysql:dbname=KCS;host=localhost";
+$user = "root";
+$pass = "";
+$dbh = new PDO($dsn, $user, $pass);
+$dbh->query("SET NAMES utf8");
+
+$nickname = htmlspecialchars($_POST["nickname"]);
+$email = htmlspecialchars($_POST["email"]);
+$goiken = htmlspecialchars($_POST["goiken"]);
+
+print $nickname;
+print "様<br />";
+print "ご意見ありがとうございました";
+print "頂いたご意見";
+print $goiken;
+print "<br />";
+print $email;
+print "にメールを送りましたのでご確認ください。";
+
+$title = "アンケート受け付けました";
+$body = $nickname . "様へ\nアンケート協力ありがとうございました";
+$body = html_entity_decode($body, ENT_QUOTES, "UTF-8");
+$head = "From: xxx@xxx.co.jp";
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+mb_send_mail($email, $body, $head);
+
+$sql = 'INSERT INTO anketo (nickname, email, goiken) VALUES ("'.$nickname .'","'. $email .'","' . $goiken .'")';
+
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$dbh = null;
+?>
+
+</body>
+</html>
